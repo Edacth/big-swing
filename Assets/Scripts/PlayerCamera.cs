97b8d92cbf19c7e0cd0cc3d150e46e9aaa283cac
Cade Anderson <cade@gamemodeone.com>
2020-03-31 21:16:15 -0700

    Import project files

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] bool smooth;
+    [SerializeField] float smoothTime = 5f;
+    [SerializeField] [Range(0, 1)] float XSensitivity;
+    [SerializeField] [Range(0, 1)] float YSensitivity;
+    [SerializeField] GameObject cameraGimble;
+    [SerializeField] Camera camera;
+    InputMaster controls;
+
+    public float MinimumX = -90F;
+    public float MaximumX = 90F;
+
+    // Camera Gimble Rotation
+    private Vector3 gimbleRot;
+    private Vector3 gimbleTargRot;
+
+    public Vector3 ForwardVector
+    {
+        get
+        {
+            return camera.transform.forward;
+        }
+    }
+
+    public Vector3 RightVector
+    {
+        get
+        {
+            return camera.transform.right;
+        }
+    }
+
+    void Awake()
+    {
+        controls = new InputMaster();
+        if (cameraGimble == null) { Debug.LogError("cameraGimble is null", this); }
+        gimbleRot = cameraGimble.transform.localRotation.eulerAngles;
+        gimbleTargRot = gimbleRot;
+    }
+
+    void Update()
+    {
+        Vector2 mouseDelta = (controls.Player.Look.ReadValue<Vector2>());
+
+        gimbleTargRot += new Vector3(-mouseDelta.y * YSensitivity, mouseDelta.x * XSensitivity, 0);
+        gimbleTargRot.x = Mathf.Clamp(gimbleTargRot.x, MinimumX, MaximumX);
+
+        if (smooth)
+        {
+            gimbleRot.x = Mathf.LerpAngle(gimbleRot.x, gimbleTargRot.x, smoothTime * Time.deltaTime);
+            gimbleRot.y = gimbleTargRot.y;
+            cameraGimble.transform.localEulerAngles = gimbleRot;
+        }
+        else
+        {
+            gimbleRot = gimbleTargRot;
+            cameraGimble.transform.eulerAngles = gimbleRot;
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] [Range(0,1)] float moveSpeed;
+    [SerializeField] PlayerCamera playerCamera;
+    InputMaster controls;
+    Rigidbody rb;
+    Vector2 moveDirection;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        controls = new InputMaster();
+        controls.Player.Fire.performed += ctx => TestInput(ctx);
+    }
+
+    void Update()
+    {
+        moveDirection = controls.Player.Move.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forwardVector = playerCamera.ForwardVector;
+        Vector3 rightVector = playerCamera.RightVector;
+        Debug.DrawRay(transform.position, forwardVector * 10, Color.red);
+
+        Vector3 moveVector = Vector3.ProjectOnPlane
+        (forwardVector * moveDirection.y * moveSpeed + rightVector * moveDirection.x * moveSpeed, Vector3.up);
+        rb.MovePosition(transform.position + moveVector);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    public void TestInput(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) { return; }
+        Debug.Log("CLick!");
+    }
+}
